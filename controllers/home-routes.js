fd8cc15c6ab5c47d82cf0ebfb097d680ab84f78d
BelenGigante <vinn_a@hotmail.com>
2023-01-29 11:43:40 -0500

    coded home routes, not tested yet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,59 @@
 const router = require('express').Router();
-const {} = require('../models/');
+const { Post, User } = require('../models/');
+const withAuth = require('../utils/auth');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage');
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['subject'],
+        },
+      ],
+    });
+    const posts= postData.map((post) => post.get({plain: true}));
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn
+    });
+  }catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    const post = postData.get({plain: true});
+    res.render('post',{
+      ...post,
+      loggedIn: req.session.loggedIn
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/comment', async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
   try {
-    res.render('comment');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/profile', async (req, res) => {
-  try {
-    res.render('profile');
+  const userData = await User.findByPk(req.session.user_id, {
+    attributes: {exclude:['password']},
+    include: [{model: Project}],
+  });
+  const user = userData.get({plain:true});
+  res.render('profile', {
+    ...user,
+    loggedIn: true
+  });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -28,7 +61,7 @@ router.get('/profile', async (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect('/profile');
     return;
   }
 
